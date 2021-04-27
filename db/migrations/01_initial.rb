e56863a1d2ad4dab1864d60e91e70b03f76ea689
Delon Newman <contact@delonnewman.name>
2021-04-27 15:00:51 -0600

    Add sessions table

--- db/migrations/01_initial.rb
+++ db/migrations/01_initial.rb
@@ -1,17 +1,11 @@
 Sequel.migration do
   change do
-    create_table? :product do
-      String :product_id, null: false, index: true
-      String :name, null: false, index: true
-      String :description, null: false
-      String :image, null: false
-      Integer :price, null: false
-      Integer :recurring
-      Time :created_at
-      Time :updated_at
-      TrueClass :active, null: false, index: true, default: true
-
-      primary_key :product_id
+    create_table :sessions do
+      String  :instant_session_id, null: false, index: true
+      String  :checkout_session_id, null: false
+      Integer :status, null: false, index: true, default: 0
+      Time    :started_at, null: false, index: true
+      Time    :ended_at, index: true
     end
   end
 end
