afe3e6ffd58a6af5dbc48dceac10e79b9371d17e
Delon Newman <contact@delonnewman.name>
2021-04-26 23:39:02 -0600

    WIP: sketching out InstantSessions workflow

--- lib/drn/mentoring/application/instant_sessions.rb
+++ lib/drn/mentoring/application/instant_sessions.rb
@@ -4,15 +4,32 @@ module Drn
       class InstantSessions < Controller
         get '/new' do |params|
           session_id = params['session_id']
+        end
+
+        # create session
+        post '/' do
           # create database record session_id:UUID, stripe_session_id:String, started_at:Time, ended_at:Time 
           # session = sessions.create!(stripe_session_id: params['session_id'])
           # redirect_to session_path(session.session_id)
         end
 
+        # show session
         get '/:id' do |params|
           # Display timer
           # Have a link to a Zoom Session
-          # And a visual studio code session
+          # Display chat & shared code editor
+        end
+
+        # update session
+        post '/:id' do
+        end
+
+        # end session
+        delete '/:id' do |params|
+          # set ended_at timestamp for session
+          # mentor should be able to update timestamp
+          # calculate quantity from started_at and ended_at
+          # redirect to checkout
         end
       end
     end
