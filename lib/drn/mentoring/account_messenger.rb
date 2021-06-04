0ab9307a284dc7935bc9be1c49821842a8f5a2ea
Delon Newman <contact@delonnewman.name>
2021-06-04 16:53:19 -0600

    Give Rubocop a chance

--- lib/drn/mentoring/account_messenger.rb
+++ lib/drn/mentoring/account_messenger.rb
@@ -1,17 +1,22 @@
+# frozen_string_literal: true
+
 module Drn
   module Mentoring
+    # Manage email communication regarding accounts
     class AccountMessenger < Mailer
       layout :mailer
-      
+
       def signup(user)
-        mail :signup, { user: user }, to: user, subject: 'Thank you for giving us a try! Please complete your registration.'
+        mail :signup, { user: user },
+             to:      user,
+             subject: 'Thank you for giving us a try! Please complete your registration.'
       end
 
       private
 
-        def activation_url(user)
-          "https://#{app.settings['DOMAIN']}/activate/#{user.id}?key=#{user.activation_key}"
-        end
+      def activation_url(user)
+        "https://#{app.settings['DOMAIN']}/activate/#{user.id}?key=#{user.activation_key}"
+      end
     end
   end
 end
